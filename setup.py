1852d2e041d6cc5a8211c6c7669af0c79f827b3c
cuauhtemoc-amdg <31977553+cuauhtemoc-amdg@users.noreply.github.com>
2017-09-14 21:24:03 -0600

    test with pyasn1 0.3.5

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=[
         "django>=1.8",
         "ldap3==2.3",
-        "pyasn1==0.3.2",
+        "pyasn1==0.3.5",
     ],
     classifiers=[
         "Development Status :: 4 - Beta",
